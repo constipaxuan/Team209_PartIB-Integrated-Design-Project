aa73ba9f4aa20a033f2102220dd03fedd5fe4723
Lazyyy-Dog <rhl33@cam.ac.uk>
2026-02-25 12:17:47 +0000

    lower floor orange and purple resistor detection and deliver

---
+++ sw/RHS_dropoff.py
@@ -0,0 +1,40 @@
+# if the car comes down from the right hand side to the lloading bay
+# Red = 0, Yellow = 1, SKIP the starting box = 2, Green = 3, Blue = 4
+target_bay = 0 
+
+if resistor_color == 0: 
+    target_bay = 0 # Red
+if resistor_color == 1: 
+    target_bay = 1 # Yellow
+if resistor_color == 3: 
+    target_bay = 3 # Green
+if resistor_color == 4: 
+    target_bay = 4 # Blue
+
+drop_off_bay = 0
+counting_line = False # This is our "latch"
+
+while True:
+    # 1. Line following code goes here (keep the car on the main spine)
+    
+    # 2. Check for branches
+    if SL == 1 and not counting_line:
+        drop_off_bay += 1
+        counting_line = True # Block further counting until we leave the line
+        print(f"Passing branch {drop_off_bay}")
+        
+    if SL == 0:
+        counting_line = False # Reset the latch once we are back on black
+        
+    # 3. Check if we reached our target
+    if drop_off_bay == target_bay:
+        print("Target reached! Turning into bay")
+        if target_bay == 0: 
+            #code to go straight and drop off in red bay
+            slot_status = [0,0,0,0,0,0] # Reset slot status for next checking
+            break
+        else:
+            #code to turn 90 degrees and drop off in other bays
+            # Insert code to turn 90 degrees and drop off
+            slot_status = [0,0,0,0,0,0] # Reset slot status for next checking
+            break # Exit loop after drop off
---
+++ sw/lowerpurple_upper_orange_R_detect.py
@@ -0,0 +1,65 @@
+#define these variables globally so that we can clear them off and reuse them for other branches
+slot_status = [0,0,0,0,0,0] #0 means unknown slot status, 1 means cleared
+resistor_color = 0 
+slot_counter = 0
+cleared_counter = slot_status.count(1)
+rack_cleared = False
+#set up laser sensor
+from turtle import distance
+
+from machine import Pin, I2C
+from libs.VL53L0X.VL53L0X import VL53L0X
+from utime import sleep
+
+
+# config I2C Bus
+i2c_bus = I2C(id=0, sda=Pin(8), scl=Pin(9)) # I2C0 on GP8 & GP9
+# print(i2c_bus.scan())  # Get the address (nb 41=0x29, 82=0x52)
+    
+# Setup vl53l0 object
+vl53l0 = VL53L0X(i2c_bus)
+vl53l0.set_Vcsel_pulse_period(vl53l0.vcsel_period_type[0], 18)
+vl53l0.set_Vcsel_pulse_period(vl53l0.vcsel_period_type[1], 14)
+
+def rec_dist_laser():
+     # Start device
+    vl53l0.start()
+
+    laser_distance = vl53l0.read()
+    print(f"Distance = {laser_distance}mm")  # Check calibration!
+        
+    # Stop device
+    vl53l0.stop()
+
+    return laser_distance
+    
+
+
+while slot_status.count(1) < 6: #number of cleared slots is less than 6
+    if SL == 1:  # Branch detected
+
+        sleep(0.1) # Short delay to debounce the sensor
+        distance = rec_dist_laser()
+        
+        if distance < 100: # resistor detected
+            # 1. Add code here to turn the car and pick up resistor
+            # 2. Once picked up resistor, mark as cleared
+            slot_status[slot_counter] = 1
+            #add code to measure resistor color and store resistor color as a variable
+            # add code to return the resistor and clear out the list
+            print(f"Slot {slot_counter} picked up and cleared.")
+        else: # Slot is empty
+            slot_status[slot_counter] = 1
+            print(f"Slot {slot_counter} was already empty. Marked cleared.")
+        
+        slot_counter += 1 # Move to next slot index for the next branch
+        
+        #add short sleep here 
+        # so it doesn't count the same branch multiple times.
+        while SL == 1:
+            pass 
+
+rack_cleared = True
+
+
+
