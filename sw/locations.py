14200cca6c4454b4b036912826dd480a5f1df009
constipaxuan <jxt21@cam.ac.uk>
2026-02-25 20:59:11 +0000

    location /path states for use in FSM

---
+++ sw/locations.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+class Path(Enum):
+    line = 1
+    junction = 2
+
+class Location(Enum):
+    unloading = 1
+    elevator_low = 2
+    rack_orange_L = 3
+    rack_purple_L = 4
+    rack_orange_U = 5
+    rack_purple_U = 6
+    elevator = 7
+    elevator_up = 8
+
+class Unloading_States(Enum):
+    none = 0
+    red = 1
+    yellow = 2
+    green = 3
+    blue = 4
+    
+class Direction(Enum):
+    cw = 1
+    acw = 2
+
+class Junctions(Enum):
+    R = 1
+    L = 2
+    RL = 3
