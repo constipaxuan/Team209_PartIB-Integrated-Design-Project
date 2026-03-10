62003105e08480beaaaa2414dff1269fe3eca295
constipaxuan <jxt21@cam.ac.uk>
2026-03-10 09:53:11 +0000

    hh

--- sw/main.py
+++ sw/main.py
@@ -5,7 +5,7 @@ from time import sleep, sleep_ms, ticks_ms, ticks_diff
 #from enum import Enum
 from behaviour import Turn_Direction, Turn_State, Mode, Start_States
 from locations import Junctions, Location, Direction
-from map_state import mapping, memory
+#from map_state import mapping, memory
 
 
 # --- CLASSES ---
