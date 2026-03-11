f5fee2d4eeb6dbea9d16dc640980f1ce3b066655
Lazyyy-Dog <rhl33@cam.ac.uk>
2026-03-11 14:17:59 +0000

    commiting main

--- sw/main.py
+++ sw/main.py
@@ -490,6 +490,36 @@ motor_r = Motor(dirPin=7, PWMPin=6)  """
         events["prev_on_junction"] = events["on_junction"]
         events["prev_on_T"] = events["on_T"]  """
 
+#Resistor detection test
+
+import upperpurple_lowerorange_R_detect as detector
+
+detector.init_laser()
+
+S1_pin = 21
+S2_pin = 20
+SL_pin = 26 
+SR_pin = 22
+
+S1_sensor = Pin(S1_pin, Pin.IN)
+S2_sensor = Pin(S2_pin, Pin.IN)
+SL_sensor = Pin(SL_pin, Pin.IN)
+SR_sensor = Pin(SR_pin, Pin.IN)
+SR = SR_sensor.value()
+
+
+while True:
+    # pretend we just crossed a junction (update events before calling)
+    # call detector using globals; pass previous laser_distance or None
+    laser_distance, slot_status, slot_counter = detector.upperP_lowO_R_detect(events, laser_distance)
+    # print distance sample and state for debugging
+    print(f"Distance reading: {laser_distance}mm")
+    print(f"Counter: {delivery['search_slot_counter']}")
+    print(f"Slot status: {delivery['slot_status']}")
+
+    sleep(2)
+    # loop continues indefinitely; break manually when done
+
 
 
 # Assumes that the turning of the car is wide enough such that the front aligns with line before the back
--- sw/unittest_R_detect.py
+++ sw/unittest_R_detect.py
@@ -1,31 +1,4 @@
-from time import sleep
-from decision import delivery, events
-import upperpurple_lowerorange_R_detect as detector
 
-detector.init_laser()
-from machine import Pin
 
-S1_pin = 21
-S2_pin = 20
-SL_pin = 26 
-SR_pin = 22
-
-S1_sensor = Pin(S1_pin, Pin.IN)
-S2_sensor = Pin(S2_pin, Pin.IN)
-SL_sensor = Pin(SL_pin, Pin.IN)
-SR_sensor = Pin(SR_pin, Pin.IN)
-SR = SR_sensor.value()
-
-while True:
-    # pretend we just crossed a junction (update events before calling)
-    # call detector using globals; pass previous laser_distance or None
-    laser_distance, slot_status, slot_counter = detector.upperP_lowO_R_detect(events, laser_distance)
-    # print distance sample and state for debugging
-    print(f"Distance reading: {laser_distance}mm")
-    print(f"Counter: {delivery['search_slot_counter']}")
-    print(f"Slot status: {delivery['slot_status']}")
-
-    sleep(2)
-    # loop continues indefinitely; break manually when done
 
 
