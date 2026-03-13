bc85a1534ce1a8f1b59a819ec03a292a7e934209
constipaxuan <jxt21@cam.ac.uk>
2026-03-13 10:39:58 +0000

    g

--- sw/i2c_scan.py
+++ sw/i2c_scan.py
@@ -1,6 +1,18 @@
 from machine import Pin, I2C
 from time import sleep_ms
+from VL53L0X import VL53L0X
 
 i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=100000)
 sleep_ms(100)
-print(i2c.scan())
+
+print("scan:", i2c.scan())
+
+tof = VL53L0X(i2c)
+print("constructed")
+
+tof.start()
+print("started")
+
+while True:
+    print(tof.read())
+    sleep_ms(100)
