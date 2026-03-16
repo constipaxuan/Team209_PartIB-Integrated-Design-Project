9d998c9af892285b4f9421ca28d4b81fd02c157a
constipaxuan <jxt21@cam.ac.uk>
2026-03-16 08:30:13 +0000

    hhh

--- sw/i2c_scan.py
+++ sw/i2c_scan.py
@@ -2,8 +2,16 @@ from machine import Pin, I2C
 from time import sleep_ms
 from libs.VL53L0X.VL53L0X import VL53L0X
 
-i2c = I2C(1, scl=Pin(11), sda=Pin(10), freq=100000)
-sleep_ms(100)
+print("testing right laser)")
+i2c_bus = I2C(id=1, sda=Pin(10), scl=Pin(11), freq=100000) # I2C1 on GP10 & GP11
+vl53l0_R = VL53L0X(i2c_bus)
+vl53l0_R.set_Vcsel_pulse_period(vl53l0_R.vcsel_period_type[0], 18)
+vl53l0_R.set_Vcsel_pulse_period(vl53l0_R.vcsel_period_type[1], 14)
+print("right scan:", vl53l0_R.scan())
 
-print("scan:", i2c.scan())
-
+print("testing left laser)")
+i2c_bus = I2C(id=0, sda=Pin(8), scl=Pin(9)) # I2C0 on GP8 & GP9
+vl53l0_L = VL53L0X(i2c_bus)
+vl53l0_L.set_Vcsel_pulse_period(vl53l0_L.vcsel_period_type[0], 18)
+vl53l0_L.set_Vcsel_pulse_period(vl53l0_L.vcsel_period_type[1], 14)
+print("right scan:", vl53l0_L.scan())
--- sw/main.py
+++ sw/main.py
@@ -103,19 +103,16 @@ ON = False
 prev_button = 0
 
 # Initialise lasers ONCE
-print("testing right laser)")
+
 i2c_bus = I2C(id=1, sda=Pin(10), scl=Pin(11), freq=100000) # I2C1 on GP10 & GP11
 vl53l0_R = VL53L0X(i2c_bus)
 vl53l0_R.set_Vcsel_pulse_period(vl53l0_R.vcsel_period_type[0], 18)
 vl53l0_R.set_Vcsel_pulse_period(vl53l0_R.vcsel_period_type[1], 14)
-print("right scan:", vl53l0_R.scan())
 
-print("testing left laser)")
 i2c_bus = I2C(id=0, sda=Pin(8), scl=Pin(9)) # I2C0 on GP8 & GP9
 vl53l0_L = VL53L0X(i2c_bus)
 vl53l0_L.set_Vcsel_pulse_period(vl53l0_L.vcsel_period_type[0], 18)
 vl53l0_L.set_Vcsel_pulse_period(vl53l0_L.vcsel_period_type[1], 14)
-print("right scan:", vl53l0_L.scan())
 
 # --- DICTIONARIES (MUTABLE - DO NOT NEED TO RETURN. HOLDS VALUES ACROSS ITERATIONS) ---
 sensors = {
